d9674e2e13cb0981506d518aa5b842c18eceb868
vladislavbakaev <bakaev.98@bk.ru>
2021-11-04 19:35:59 +0300

    delete unused files

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AllMission from '@/views/AllMissionPage.vue'
-import MissionCreate from '@/views/NewMissionPage.vue'
-import LogCreate from '@/views/NewLogsPage.vue'
-import MissionPage from '@/views/MissionPage.vue'
 import MainMapMission from "@/views/MainMapMission.vue"
 
 const routes = [
   {
     path: '/',
-    name: 'AllMission',
-    component: AllMission
-  },
-  {
-    path: '/mission_create',
-    name: 'MissionCreate',
-    component: MissionCreate
-  },
-  {
-    path: '/log_create',
-    name: 'LogCreate',
-    component: LogCreate
-  },
-  {
-    path: '/mission/:id',
-    component: MissionPage
-  },
-  {
-    path: '/main',
+    name: 'MainMapMission',
     component: MainMapMission
   }
 ]
